b4f24dc02569663fbbaec3a30cf430316306b704
ysu-gjx <252932110@qq.com>
2020-11-12 12:49:04 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    hello world
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~common/stylus/variable"
+
+  #app
+    color: $color-theme
+</style>
