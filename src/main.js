e23047d8e631aca6254d0b851620dd65cc48f873
ysu-gjx <252932110@qq.com>
2020-11-14 18:06:11 +0800

    推荐页面 添加slider scroll  loading 组件

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
-import fastclick from 'fastclick'
+// import fastclick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
 import router from 'src/router'
 
 import 'common/stylus/index.styl'
 
 Vue.config.productionTip = false
 
+Vue.use(VueLazyLoad, {
+  loading: require('common/image/default.png'),
+})
+
 // 解决移动端点击300ms延迟问题
-fastclick.attach(document.body)
+// fastclick.attach(document.body)
 
 new Vue({
   render: h => h(App),
