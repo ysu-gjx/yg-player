94b4725556687ef3baa3a187086d83ef44390adb
ysu-gjx <252932110@qq.com>
2022-03-06 01:04:45 +0800

    url

--- src/service/base.js
+++ src/service/base.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const ERR_OK = 0
-const baseURL = process.env.NODE_ENV === 'production' ? 'http://ustbhuangyi.com/music-next/' : '/'
+// const baseURL = process.env.NODE_ENV === 'production' ? 'http://ustbhuangyi.com/music-next/' : '/'
+const baseURL = process.env.NODE_ENV === 'production' ? 'http://192.168.31.147:8080/' : '/'
 
 axios.defaults.baseURL = baseURL
 
